8ee740c59b34501b724031d74b3d10bb2f52aa2f
mattgrannary <me@mattgrannary.com>
2015-09-08 21:29:17 -0400

    fixing validation mixin test

--- tests/unit/mixins/validate-test.js
+++ tests/unit/mixins/validate-test.js
@@ -2,15 +2,32 @@
 import { expect } from 'chai';
 import {
   describe,
-  it
+  it,
+  before
 } from 'mocha';
 import Ember from 'ember';
-import ValidateMixin from 'validity/mixins/validate';
+import ValidateMixin from 'validity/mixins/components/validate';
 
 describe('ValidateMixin', function() {
-  // Replace this with your real tests.
+  let ValidateObject;
+
+  before(function() {
+    ValidateObject = Ember.Object.extend(ValidateMixin,{
+      validator: {
+        validate(type,value) {
+          return new Ember.RSVP.Promise( (resolve, reject)=> {
+            if (value === 'fail') {
+              reject({errors: [{message: 'Validation failed'}]});
+            } else {
+              resolve();
+            }
+          });
+        }
+      }
+    });
+  });
+
   it('works', function() {
-    var ValidateObject = Ember.Object.extend(ValidateMixin);
     var subject = ValidateObject.create();
     expect(subject).to.be.ok;
   });
