3d093769f300796be9d1d923eb0cd9935547b0c6
mattgrannary <me@mattgrannary.com>
2015-09-28 21:07:23 -0400

    updated presence validator

--- addon/validators/presence.js
+++ addon/validators/presence.js
@@ -3,22 +3,11 @@ import ValidatorBase from 'validity/validators/base';
 
 export default ValidatorBase.extend({
 
-  errorMsg: {
-    message: 'This is a required field.'
-  },
+  defaultMessage: 'This is a required field.',
 
   run() {
     const value = this.get('value');
-    const options = this.get('options');
-
-    let message;
-    if (options && options.message) {
-      message = {
-        message: options.message
-      };
-    } else {
-      message = this.get('errorMsg');
-    }
+    const message = this.get('message');
 
     return new Ember.RSVP.Promise( (resolve, reject)=> {
       if (value && value.length) {
